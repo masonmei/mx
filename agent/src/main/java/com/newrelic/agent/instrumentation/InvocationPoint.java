c4a3d10cd8f22b4dc0e5a74ba0d8462e7cfad869
Mason <dongxu.m@gmail.com>
2015-07-28 12:08:36 +0800

    New changelist

--- agent/src/main/java/com/newrelic/agent/instrumentation/InvocationPoint.java
+++ agent/src/main/java/com/newrelic/agent/instrumentation/InvocationPoint.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.newrelic.agent.instrumentation;
 
 import java.lang.reflect.InvocationHandler;
@@ -12,6 +17,7 @@ import com.newrelic.agent.dispatchers.Dispatcher;
 import com.newrelic.agent.tracers.ClassMethodSignature;
 import com.newrelic.agent.tracers.EntryInvocationHandler;
 import com.newrelic.agent.tracers.PointCutInvocationHandler;
+import com.newrelic.agent.tracers.Tracer;
 import com.newrelic.agent.tracers.TracerFactory;
 
 final class InvocationPoint implements InvocationHandler {
@@ -30,37 +36,34 @@ final class InvocationPoint implements InvocationHandler {
 
     public static InvocationHandler getStacklessInvocationHandler(ClassMethodSignature classMethodSignature,
                                                                   EntryInvocationHandler tracerFactory) {
-        return new StacklessInvocationPoint(classMethodSignature, tracerFactory);
+        return new InvocationPoint.StacklessInvocationPoint(classMethodSignature, tracerFactory);
     }
 
     public static InvocationHandler getInvocationPoint(PointCutInvocationHandler invocationHandler,
                                                        TracerService tracerService,
                                                        ClassMethodSignature classMethodSignature, boolean ignoreApdex) {
-        if ((invocationHandler instanceof EntryInvocationHandler)) {
+        if (invocationHandler instanceof EntryInvocationHandler) {
             return getStacklessInvocationHandler(classMethodSignature, (EntryInvocationHandler) invocationHandler);
-        }
-        if ((invocationHandler instanceof TracerFactory)) {
+        } else if (invocationHandler instanceof TracerFactory) {
             return new InvocationPoint(tracerService, classMethodSignature.intern(), (TracerFactory) invocationHandler,
                                               ignoreApdex);
+        } else {
+            Agent.LOG.finest("Unable to create an invocation handler for " + invocationHandler);
+            return ignoreApdex ? IgnoreApdexInvocationHandler.INVOCATION_HANDLER
+                           : NoOpInvocationHandler.INVOCATION_HANDLER;
         }
-
-        Agent.LOG.finest("Unable to create an invocation handler for " + invocationHandler);
-        if (ignoreApdex) {
-            return IgnoreApdexInvocationHandler.INVOCATION_HANDLER;
-        }
-        return NoOpInvocationHandler.INVOCATION_HANDLER;
     }
 
     public ClassMethodSignature getClassMethodSignature() {
-        return classMethodSignature;
+        return this.classMethodSignature;
     }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         try {
-            if (ignoreApdex) {
-                Transaction transaction = Transaction.getTransaction();
-                if (transaction != null) {
-                    Dispatcher dispatcher = transaction.getDispatcher();
+            if (this.ignoreApdex) {
+                Transaction t = Transaction.getTransaction();
+                if (t != null) {
+                    Dispatcher dispatcher = t.getDispatcher();
                     if (dispatcher != null) {
                         dispatcher.setIgnoreApdex(true);
                         if (Agent.LOG.isLoggable(Level.FINER)) {
@@ -71,23 +74,25 @@ final class InvocationPoint implements InvocationHandler {
                 }
             }
 
-            Object t = tracerService.getTracer(tracerFactory, classMethodSignature, args[0], (Object[]) args[1]);
-            if (t == null) {
-                ;
+            Tracer t1 = this.tracerService
+                                .getTracer(this.tracerFactory, this.classMethodSignature, args[0], (Object[]) args[1]);
+            if (t1 == null) {
+                t1 = null;
             }
+
+            return t1;
+        } catch (Throwable var7) {
+            Agent.LOG.log(Level.FINEST, "Tracer invocation error", var7);
             return null;
-        } catch (Throwable t) {
-            Agent.LOG.log(Level.FINEST, "Tracer invocation error", t);
         }
-        return null;
     }
 
     public String toString() {
-        return MessageFormat.format("{0} {1}", new Object[] {classMethodSignature});
+        return MessageFormat.format("{0} {1}", this.classMethodSignature);
     }
 
     public TracerFactory getTracerFactory() {
-        return tracerFactory;
+        return this.tracerFactory;
     }
 
     private static final class StacklessInvocationPoint implements InvocationHandler {
@@ -101,7 +106,7 @@ final class InvocationPoint implements InvocationHandler {
         }
 
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            tracerFactory.handleInvocation(classMethodSignature, args[0], (Object[]) args[1]);
+            this.tracerFactory.handleInvocation(this.classMethodSignature, args[0], (Object[]) args[1]);
             return null;
         }
     }
